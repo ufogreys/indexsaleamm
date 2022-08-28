e692254eac0e156a893cbb9d91ac57c0578ba000
indexsale <112284961+indexsale@users.noreply.github.com>
2022-08-27 21:27:12 -0300

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -17,7 +17,7 @@ const Menu: React.FC = props => {
   return (
     <UikitMenu
       links={links}
-      priceLink="https://www.coingecko.com/en/coins/CryptoFan"
+      priceLink="https://www.coingecko.com/en/coins/indexsale"
       account={account as string}
       login={(connectorId: ConnectorId) => {
         if (connectorId === 'walletconnect') {
